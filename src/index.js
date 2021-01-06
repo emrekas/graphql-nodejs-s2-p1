caf8c86b9f355964f5da1823811ed95fd966cb51
emrekas <y.emrekas@outlook.com>
2021-01-06 13:20:55 +0300

    named ve default export ile örnek yapıldı.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import myCurrentLocation, { getGreeting, message, name } from './myModule';
+import myAddFunction, { subtract } from "./math";
 
-console.log(message);
-console.log(name);
-console.log(myCurrentLocation);
-console.log(getGreeting('Aylin'));
+
+console.log(myAddFunction(2, 2));
+console.log(subtract(2 - 2));

2be3aa1e7f0497cbec8116386feaba558a107551
emrekas <y.emrekas@outlook.com>
2021-01-06 13:15:59 +0300

    babel eklendi - import/export ile calisildi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+import myCurrentLocation, { getGreeting, message, name } from './myModule';
+
+console.log(message);
+console.log(name);
+console.log(myCurrentLocation);
+console.log(getGreeting('Aylin'));
